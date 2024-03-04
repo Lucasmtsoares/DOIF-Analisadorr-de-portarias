933c93fada8de1c2e4afd9a42a85c96acd3c62c5
Lucsmtsoares <lmsl3@aluno.ifal.edu.br>
2024-03-04 10:13:35 -0300

    test impresson [corretion][4]

--- api/controllers/default.py
+++ api/controllers/default.py
@@ -1,5 +1,5 @@
 #from app.core.analitic_date import Analitic
-from models.dados import res as result
+from api.models.dados import res as result
 from flask import render_template
 from app import app
 import json
