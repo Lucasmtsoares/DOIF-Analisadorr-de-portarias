3139c182efb828f02ae3a14407af1074361f32dc
Lucsmtsoares <lmsl3@aluno.ifal.edu.br>
2024-10-05 15:33:08 -0300

    Import removal of deleted files

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,5 +1,3 @@
-from app.core.analitic_date import Analitic
-from app.models.dados import res as result
 from flask import render_template, request
 from app import app
 import json
@@ -14,27 +12,10 @@ def page_de_busca():
 
 @app.route("/painel-de-analise")
 def painel_de_analise():
-            
     return render_template("painel_de_analise.html")
 
 @app.route("/painel-grafico")
 def painel_grafico():
-            
     return render_template("painel_grafico.html")
 
 
-
-@app.route("/analisar-portarias", methods=['POST'])
-def analisar_portaria():
-    if_ = request.form['if']
-    date = request.form['date']
-    
-    #res = result
-    if_analisar = Analitic()
-    res = if_analisar.analitic(if_, date)
-    
-    
-   
-    print(res)
-        
-    return render_template("analitic.html", res=res)
